5bcba781905a51d9ffd33a45e1cac87988a75ed6
Leonardo_Cosmos <kylin_cl@hotmail.com>
2016-07-10 22:32:27 +0800

    Implement: assembly annotation

---
+++ src/org/pseudoscript/assembly/annotation/Executor.java
@@ -0,0 +1,14 @@
+package org.pseudoscript.assembly.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Executor {
+
+	String name();
+	
+}
