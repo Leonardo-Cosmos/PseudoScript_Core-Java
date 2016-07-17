0e132ca3bae8b4b7e982fd421c78eb009cc97d89
Leonardo_Cosmos <kylin_cl@hotmail.com>
2016-07-17 23:51:00 +0800

    Implement: Script
    Add exception for interpreter.

---
+++ src/org/pseudoscript/program/ExecutorNotFoundException.java
@@ -0,0 +1,5 @@
+package org.pseudoscript.program;
+
+public class ExecutorNotFoundException extends Exception {
+
+}
---
+++ src/org/pseudoscript/program/OperationNotFoundException.java
@@ -0,0 +1,5 @@
+package org.pseudoscript.program;
+
+public class OperationNotFoundException extends Exception {
+
+}
