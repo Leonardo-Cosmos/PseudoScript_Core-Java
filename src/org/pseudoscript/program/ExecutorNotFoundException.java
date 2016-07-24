ec963a7b5eb3693401b63ee0db69fe80393daedf
Leonardo_Cosmos <kylin_cl@hotmail.com>
2016-07-24 17:04:58 +0800

    Refactor: add serial version UID

--- src/org/pseudoscript/program/ExecutorNotFoundException.java
+++ src/org/pseudoscript/program/ExecutorNotFoundException.java
@@ -2,4 +2,9 @@ package org.pseudoscript.program;
 
 public class ExecutorNotFoundException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3591182556423436423L;
+
 }
--- src/org/pseudoscript/program/OperationNotFoundException.java
+++ src/org/pseudoscript/program/OperationNotFoundException.java
@@ -2,4 +2,9 @@ package org.pseudoscript.program;
 
 public class OperationNotFoundException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2603449091843174022L;
+
 }
